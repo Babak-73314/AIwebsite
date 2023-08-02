d610b1e4fa175706aee62072a83576971cc06abb
Babak M <babak73314@outlook.com>
2023-08-02 16:57:20 +0330

    Add project files.

---
+++ AIwebsite/Program.cs
@@ -0,0 +1,65 @@
+﻿using AIwebsite.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using System.Globalization;
+using AIwebsite.Shared;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Localization;
+using AIwebsite.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// Add services to the container.
+
+var cultureInfo = new CultureInfo("tr");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+
+
+var supportedCultures = new[] { "en-US", "tr" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[1]) // This sets Turkish as the default language
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(supportedCultures[1]);
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(supportedCultures[1]);
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NGaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXldcHVRR2RdVER1WUQ=");
+
+// Register Syncfusion services
+builder.Services.AddSyncfusionBlazor();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ChatService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseRequestLocalization(localizationOptions);
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ AIwebsite/Services/ChatService.cs
@@ -0,0 +1,50 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using System;
+
+namespace AIwebsite.Services
+{
+    public class ChatService
+    {
+        private OpenAIClient client;
+
+        public ChatService()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+
+            // If the environment variable is not found, fallback to the hardcoded value.
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                    apiKey = "1b262ef480aa4b338c5106460ff9ca29"; // Replace this with your actual API key.
+            }
+
+            //client = new OpenAIClient(
+            //    new Uri("https://azureopenaibabak.openai.azure.com/openai/deployments/DeploymentBabak/chat/completions?api-version=2023-03-15-preview/"),
+            //    new AzureKeyCredential(apiKey)
+            //);
+            client = new OpenAIClient(
+    new Uri("https://azureopenaibabak.openai.azure.com/"),
+    new AzureKeyCredential(apiKey)
+            );
+
+        }
+
+        public async Task<ChatCompletions> GetChatCompletionsAsync(ChatMessage message)
+        {
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(
+                "DeploymentBabak",
+                new ChatCompletionsOptions()
+                {
+                    Messages = { message },
+                    Temperature = 1f,
+                    MaxTokens = 400,
+                    NucleusSamplingFactor = 0.95f,
+                    FrequencyPenalty = 0,
+                    PresencePenalty = 0,
+                });
+
+            return response.Value;
+        }
+    }
+
+}
